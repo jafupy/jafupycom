50101d03afe647324af54c26aa3497fa5c459770
Jacob <104758482+JaFu-py@users.noreply.github.com>
2024-08-03 15:08:52 +0100

    h

--- src/app/(public)/r/[slug]/page.tsx
+++ src/app/(public)/r/[slug]/page.tsx
@@ -1,18 +1,3 @@
-import {} from "react";
-import { useMutation } from "@tanstack/react-query";
-import { useRouter } from "next/router";
-import { slug } from "../../actions/links";
-
 export default function Page() {
-  const [pageinfo, setPageinfo] = useState({});
-  const router = useRouter();
-  const { slug } = router.query;
-
-  const {} = useMutation({
-    mutateFn: async () => {
-      const data = await slug(slug);
-      setPageinfo(data);
-    },
-  });
-  return <div></div>;
+  return <div>Under construction</div>;
 }
