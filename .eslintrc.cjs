bb841392f42433ec13401beb4a79b1166e85130e
Jacob <104758482+JaFu-py@users.noreply.github.com>
2024-08-03 15:10:33 +0100

    h

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,9 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
   parser: "@typescript-eslint/parser",
-
+  parserOptions: {
+    project: false,
+  },
   plugins: ["@typescript-eslint", "drizzle"],
   extends: [
     "next/core-web-vitals",
