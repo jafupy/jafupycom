1c7f5ff151b215ac96a573470ec716c07a65a77b
Jacob <104758482+JaFu-py@users.noreply.github.com>
2024-08-03 22:42:29 +0100

    h

--- src/app/(public)/blog/[year]/[month]/[id]/page.tsx
+++ src/app/(public)/blog/[year]/[month]/[id]/page.tsx
@@ -1,6 +1,5 @@
 "use server";
 
-import CodeBlock from "@/components/jaf-ui/code";
 import { createServerClient } from "@/utils/supabase/server";
 
 import { MDXRemote } from "next-mdx-remote";
@@ -9,7 +8,8 @@ import { toast } from "sonner";
 export default async function Page({ params }: { params: { year: string; month: string; id: string } }) {
   const supabase = createServerClient();
   const { data: _data, error } = await supabase.from("blog").select("*").eq("slug", params.id);
-  const data = _data[0];
+
+  const data = _data[0] || {};
   if (error) {
     toast.error(error.message);
     throw error;
@@ -19,8 +19,6 @@ export default async function Page({ params }: { params: { year: string; month: 
     <article>
       <h1>{data?.title}</h1>
       <MDXRemote source={data.body} />
-      {/* <CodeBlock code={router} language="json" /> */}
-      {/* <CodeBlock code={JSON.stringify({ data, params, error }, null, 2)} language="json" /> */}
     </article>
   );
 }
