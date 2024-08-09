1b4980c6fc565a684ba4496ff8643dc4dbdc2eec
Jacob <104758482+JaFu-py@users.noreply.github.com>
2024-08-09 09:58:32 +0100

    Footer bug fixes #1

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -19,29 +19,29 @@ export default function Footer() {
   }, [theme, newTheme]);
 
   return (
-    <footer class="flex w-full items-center justify-between border-t border-t-slate-500/20 px-5 py-12">
-      <ul class="order-2 flex flex-row items-center justify-center gap-4">
+    <footer className="flex w-full items-center justify-between border-t border-t-slate-500/20 px-5 py-12">
+      <ul className="order-2 flex flex-row items-center justify-center gap-4">
         <li>
-          <a href="https://x.com/jafupy" class="bla text-slate-500">
-            <span class="sr-only">X</span>
+          <a href="https://x.com/jafupy">
+            <span className="sr-only">X</span>
             <svg
               fill="currentColor"
               viewBox="0 0 24 24"
               aria-hidden="true"
-              class="h-5 w-5"
+              className="h-5 w-5"
             >
               <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z"></path>
             </svg>
           </a>
         </li>
         <li>
-          <a href="#" class="bla text-slate-500">
-            <span class="sr-only">GitHub</span>
+          <a href="#" className="bla text-slate-500">
+            <span className="sr-only">GitHub</span>
             <svg
               fill="currentColor"
               viewBox="0 0 24 24"
               aria-hidden="true"
-              class="h-5 w-5"
+              className="h-5 w-5"
             >
               <path
                 fill-rule="evenodd"
@@ -70,7 +70,7 @@ export default function Footer() {
           </Button>
         </li>
       </ul>
-      <p class="text-center text-xs text-slate-500">
+      <p className="text-center text-xs text-slate-500">
         © {new Date().getFullYear()} Jacob / @JaFu.py. All rights reserved.
       </p>
     </footer>
